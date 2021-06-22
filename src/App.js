c6ed9e24c2300ec06f52442b7cfb8a6f3fe79531
mlfernandez <mlfernandez@gmail.com>
2021-06-22 14:00:03 +0200

    create redux folders and components, login, home, header

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
+
 import './App.scss';
+import {BrowserRouter, Route, Switch} from 'react-router-dom';
+import Header from './components/Header/Header';
+
+/* import Footer from './components/Footer/Footer';
+import Login from './containers/Login/Login';
+import Home from './containers/Home/Home';
+import Profile from './containers/Profile/Profile';
+import Register from './containers/Register/Register';
+import Demo from './containers/Form/Form'; */
+
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <BrowserRouter>
+
+        <Header/>
+
+            <Switch>
+              <Route path="/" exact component={}/>
+              <Route path="/login" exact component={Login}/>
+{/*               <Route path="/profile" exact component={Profile}/>
+              <Route path="/register" exact component={Register}/>
+              <Route path="/form" exact component={Demo}/>
+                     */}
+            </Switch>
+
+        
+      </BrowserRouter>
+
     </div>
   );
 }
 
 export default App;
+
---
+++ src/containers/Home/Home.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+/* import CarouselSlider from '../../components/Carousel/Carousel'; */
+
+/* import  Footer  from '../../components/Footer/Footer'; */
+
+
+
+
+
+
+const Home = () => {
+    return (
+        <div className="HomeDiv">
+           
+\
+        </div>
+    );
+}
+export default Home;
