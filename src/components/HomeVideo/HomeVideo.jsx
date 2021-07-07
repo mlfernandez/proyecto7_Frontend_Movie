0bd86e3cb43e2ffb7a02acf5ace0eb5173fb17ef
mlfernandez <mlfernandez@gmail.com>
2021-07-07 22:10:43 +0200

    change video src to test heroku

--- src/components/HomeVideo/HomeVideo.jsx
+++ src/components/HomeVideo/HomeVideo.jsx
@@ -4,16 +4,16 @@ import './HomeVideo.scss';
 
 import React from 'react';
 import { Player } from 'video-react';
-import Trailer from '../../images/Trailer.mp4'
+/* import Trailer from '../../images/Trailer.mp4' */
  
 export default props => {
   return (
       <div className="Trailer">
-
+{/* 
 <video width="100%" height="100%" autoPlay controls>
   <source src={Trailer} type="video/mp4"/>
 
-</video>
+</video> */}
 {/*     <Player controls autoplay>
       <source src={Trailer} />
     </Player> */}
