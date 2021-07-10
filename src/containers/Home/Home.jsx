aeb19d93b1efa0d7f49c1d891dd6717fb0bcd776
mlfernandez <mlfernandez@gmail.com>
2021-07-10 16:00:06 +0200

    aws fixing video

--- src/containers/Home/Home.jsx
+++ src/containers/Home/Home.jsx
@@ -1,6 +1,5 @@
 import React, { useEffect, useState } from "react";
 import {useHistory} from 'react-router-dom';
-import Video from "../../components/HomeVideo/HomeVideo";
 import './Home.scss';
 
 // traer el video trailer Todo Moriran al home
@@ -14,15 +13,12 @@ const Home = () => {
         <div className="HomeDiv">
 
             <div>     
-            {/* <iframe src="https://player.vimeo.com/video/569479357?autoplay=1" width="1280" height="720" frameborder="0" allow="autoplay; fullscreen; picture-in-picture" allowfullscreen></iframe> */}
     
-            <iframe src="https://player.vimeo.com/video/569479357?autoplay=1;badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479" width="854" height="480" frameborder="0" allow="autoplay; fullscreen; picture-in-picture" allowfullscreen title="Todos moriran trailer"></iframe>
+            <iframe src="https://player.vimeo.com/video/569479357?autoplay=1&amp;badge=0&amp;player_id=0&amp;app_id=58479" width="854" height="480" frameborder="0" allow="autoplay; fullscreen; picture-in-picture" allowfullscreen title="Todos moriran trailer"></iframe>
             
-               {/*  <Video/> */}
-
             { setTimeout(() => {
                 history.push("/datacontainer")
-                }, 30000)}
+                }, 35000)}
          
                 </div>
 
